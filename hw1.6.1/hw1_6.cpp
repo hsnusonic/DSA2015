edddc21ea0c63b4b48307c0a1e5504ef720540ae
Sonic <hsnusonic@gmail.com>
2015-03-22 20:57:32 +0800

    change functions to count iterations

--- hw1.6.1/hw1_6.cpp
+++ hw1.6.1/hw1_6.cpp
@@ -2,37 +2,40 @@
 
 using namespace std;
 
-int gcd_by_reverse_search(const int& a, const int& b){
+int gcd_by_reverse_search(const int& a, const int& b, int& count){
   for (int i = a < b ? a : b; i >= 1; i--){
+    count ++;
     if (a % i == 0 && b % i == 0)
       return i;
   }
   return 0;
 }
 
-int gcd_by_filter(const int& a, const int& b){
+int gcd_by_filter(const int& a, const int& b, int& count){
   int min = a < b ? a : b;
   for (int i = 2; i <= min; i++){
+    count ++;
     if (a % i == 0 && b % i == 0)
-      return i * gcd_by_filter(a/i, b/i);
+      return i * gcd_by_filter(a/i, b/i, count);
   }
   return 1;
 }
 
-int gcd_by_filter_faster_internal(const int& a, const int& b, int s){
+int gcd_by_filter_faster_internal(const int& a, const int& b, int s, int& count){
   int min = a < b ? a : b;
   for (int i = s; i <= min; i++){
+    count ++;
     if (a % i == 0 && b % i == 0)
-      return i * gcd_by_filter_faster_internal(a/i, b/i, i);
+      return i * gcd_by_filter_faster_internal(a/i, b/i, i, count);
   }
   return 1;
 }
 
-int gcd_by_filter_faster(const int& a, const int& b){
-  return gcd_by_filter_faster_internal(a, b, 2);
+int gcd_by_filter_faster(const int& a, const int& b, int& count){
+  return gcd_by_filter_faster_internal(a, b, 2, count);
 }
 
-int gcd_by_binary(const int& a, const int& b){
+int gcd_by_binary(const int& a, const int& b, int& count){
   int n, m;
   int ans = 1;
 
@@ -43,6 +46,7 @@ int gcd_by_binary(const int& a, const int& b){
   }
 
   while(n != 0 && m != 0){
+    count ++;
     if (n % 2 == 0){
       if (m % 2 == 0){
         ans *= 2;
@@ -64,7 +68,7 @@ int gcd_by_binary(const int& a, const int& b){
   return (n * ans);
 }
 
-int gcd_by_euclid(const int& a, const int& b){
+int gcd_by_euclid(const int& a, const int& b, int& count){
   int n, m;
 
   if (a < b){
@@ -75,6 +79,7 @@ int gcd_by_euclid(const int& a, const int& b){
   }
 
   while (m % n != 0){
+    count ++;
     int tmp;
     tmp = n;
     n = m % n;
@@ -86,14 +91,25 @@ int gcd_by_euclid(const int& a, const int& b){
 
 int main(void){
   int a, b;
+  int count;
   cin >> a;
   while (a > 0) {
     cin >> b;
-    cout << "Case (" << a << ", " << b << "): GCD-By-Reverse-Search = 1, taking " << gcd_by_reverse_search(a,b) << " iterations" << endl;
-    cout << "Case (" << a << ", " << b << "): GCD-By-Filter = 1, taking " << gcd_by_filter(a,b) << " iterations" << endl;
-    cout << "Case (" << a << ", " << b << "): GCD-By-Filter-Faster = 1, taking " << gcd_by_filter_faster(a,b) << " iterations" << endl;
-    cout << "Case (" << a << ", " << b << "): GCD-By-Binary = 1, taking " << gcd_by_binary(a,b) << " iterations" << endl;
-    cout << "Case (" << a << ", " << b << "): GCD-By-Euclid = 1, taking " << gcd_by_euclid(a,b) << " iterations" << endl;
+    count = 0;
+    gcd_by_reverse_search(a, b, count);
+    cout << "Case (" << a << ", " << b << "): GCD-By-Reverse-Search = 1, taking " << count << " iterations" << endl;
+    count = 0;
+    gcd_by_filter(a, b, count);
+    cout << "Case (" << a << ", " << b << "): GCD-By-Filter = 1, taking " << count << " iterations" << endl;
+    count = 0;
+    gcd_by_filter_faster(a, b, count);
+    cout << "Case (" << a << ", " << b << "): GCD-By-Filter-Faster = 1, taking " << count << " iterations" << endl;
+    count = 0;
+    gcd_by_binary(a, b, count);
+    cout << "Case (" << a << ", " << b << "): GCD-By-Binary = 1, taking " << count << " iterations" << endl;
+    count = 0;
+    gcd_by_euclid(a, b, count);
+    cout << "Case (" << a << ", " << b << "): GCD-By-Euclid = 1, taking " << count << " iterations" << endl;
     cin >> a;
   }
   return 0;
